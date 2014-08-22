801c8c8bafa1f37786a1541c4504d41429456120
U-ACE-VAIO\ACE <ACE@ACE-VAIO.(none)>
2014-08-22 17:36:35 +0800

    timestamp

--- src/iscas/seagochen/analysis/MQReportAnalysisMain.java
+++ src/iscas/seagochen/analysis/MQReportAnalysisMain.java
@@ -3,9 +3,19 @@ package iscas.seagochen.analysis;
 import iscas.seagochen.frontend.socket.HisenseMQAnalysisSocket;
 import iscas.seagochen.frontend.socket.HisenseMQSocket;
 
+import java.awt.print.Printable;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 import javax.jms.JMSException;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class MQReportAnalysisMain {
 	
@@ -31,16 +41,32 @@ public class MQReportAnalysisMain {
 			System.exit( 1 );
 		}
 		
-		while( true )
+		PrintWriter out = null;
+		File f = new File("e:\\record.txt");
+		
+		try {
+			out = new PrintWriter(new BufferedWriter(new FileWriter(f,true)));
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		for( int i = 0; i < 1000; i++ )
 		{
+			long currentTime=System.currentTimeMillis();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String date = sdf.format(new Date(currentTime));
 			try {
-				System.out.println( new String(socket.recv("UTF-8"), "UTF-8") );
+				out.println(date + "\t" + new String(socket.recv("UTF-8"), "UTF-8") );
+				System.out.println(date + "\t" + new String(socket.recv("UTF-8"), "UTF-8") );
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			} catch (JMSException e) {
 				e.printStackTrace();
 			}
 		}
+		out.close();
 	}
 	
 	public static void main( String argv[] )
